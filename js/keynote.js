43561f3cd91f3f2901c0cc58c2cb5831ad3dd18d
Kos <kos@cloudigit.com>
2013-08-14 16:32:31 +0800

    讓時間變成動態的

--- js/keynote.js
+++ js/keynote.js
@@ -16,33 +16,37 @@ $(function() { //要用jQuery的語法，要把程式包在這裡面
 	//留言
 	function leaveNote() {
 
-		var usernameElement = $("#username"); //取出HTML中ID為content的元素
-		var noteElement = $("#note"); //取出HTML中ID為note的元素
-		var contentElement = $("#content"); //取出HTML中ID為content的元素
-
-		var username = usernameElement.val(); //取任何表單元素的值
-		var note = noteElement.val(); //取任何表單元素的值
-		var orginalContent = contentElement.html(); //取出該元素的HTML內容
+		var username = $("#username").val(); //取任何表單元素的值
+		var note = $("#note").val(); //取任何表單元素的值
 
 		if (note != "") {
 
-			var isFirstTime = orginalContent == ""; //布林值 true / false
-			var dateTime = new Date;
-			var day = dateTime.getFullYear()+'/'+dateTime.getMonth()+'/'+dateTime.getDate()+'   '+dateTime.getHours()+':'+dateTime.getMinutes()+':'+dateTime.getSeconds();;
-			// var noteContent = username + " : " + note + "  (" + day+ ")";
+			var noteHtml = getNoteHtml(username, note, getTime());
 
-			var source   = $("#entry-template").html();
-			var template = Handlebars.compile(source);
+			$("#content").prepend(noteHtml); //把新的內容塞到content的最上邊
 
-			var item = {
-				name: username,
-				note: note
-			};
-
-			var newContent = template(item);
-			$("#content").prepend($(newContent)); //把新的內容塞到content的最上邊
-
-	        noteElement.val(""); //清除留言文字
+	        $("#note").val(""); //清除留言文字
 		}
+	}
+
+	function getNoteHtml(username, note, postTime) {
+		var source   = $("#message-template").html();
+		var template = Handlebars.compile(source);
+
+		var item = {
+			"name" : username,
+			"note" : note,
+			"postTime" : postTime
+		};
+
+		return template(item);
+	}
+
+	function getTime() {
+		var dateTime = new Date;
+		var day = dateTime.getFullYear() + '/' + dateTime.getMonth() + '/' + dateTime.getDate() + '   ' + 
+			dateTime.getHours()+ ':' + dateTime.getMinutes() + ':' + dateTime.getSeconds();
+
+		return day;
 	}
 });
